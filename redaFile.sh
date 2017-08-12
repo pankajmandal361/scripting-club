63b4e3805e834137342ec9fa2848c625119a0a16
pankajmandal361 <pankajmandal361@gmail.com>
2017-08-13 00:35:59 +0530

    Create redaFile.sh

---
+++ redaFile.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+clear
+#
+file=myfile.txt
+#
+if [ -n "$file" ]
+then
+    echo
+else
+     clear
+     echo "Sorry file not available."
+     echo "Exiting the script."
+     echo "Bye...
+     sleep 2
+     clear
+     exir 32
+fi
+#
+cat $file | while read line
+do
+  sleep 1
+  echo $line
+   sleep 1
+done
+#
+#End of the script.
